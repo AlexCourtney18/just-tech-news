b00d8ccd1266bf004b87476f5d5fca1c5eba9954
Alex <lex.courtney18@gmail.com>
2022-08-22 11:07:48 -0500

    14.1.6

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
-            console,log(err);
+            console.log(err);
             res.status(500).json(err);
         });
 });
