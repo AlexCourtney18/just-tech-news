900b6c80209056a16f67d93d0fef8ea9a2491617
Alex <lex.courtney18@gmail.com>
2022-08-24 15:29:03 -0500

    fix bug for heroku

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const withAuth = require('../../utils/auth');
 
 // get /api/users
 router.get('/', (req, res) => {
